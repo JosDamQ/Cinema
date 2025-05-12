437fa2f67819229ab693609d15ed51470b89c6f4
Fabian Herrera <josefabianherrera75@gmail.com>
2025-05-12 15:59:24 -0600

    Integracion de la pantalla de peliculas

--- ProyectMovies/Salas.h
+++ ProyectMovies/Salas.h
@@ -35,6 +35,14 @@ namespace ProyectMovies {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ colCodigo;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ colNombre;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ colCapacidad;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ colUbicacion;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ colNombreEncargado;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ colTelefonoEncargado;
+
 	protected:
 
 	private:
@@ -51,25 +59,79 @@ namespace ProyectMovies {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->colCodigo = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->colNombre = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->colCapacidad = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->colUbicacion = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->colNombreEncargado = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->colTelefonoEncargado = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(252, 140);
+			this->label1->Location = System::Drawing::Point(660, 401);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(267, 30);
+			this->label1->Size = System::Drawing::Size(120, 13);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Esta es la vista de salas";
 			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(6) {
+				this->colCodigo,
+					this->colNombre, this->colCapacidad, this->colUbicacion, this->colNombreEncargado, this->colTelefonoEncargado
+			});
+			this->dataGridView1->Location = System::Drawing::Point(12, 197);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(643, 217);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// colCodigo
+			// 
+			this->colCodigo->HeaderText = L"Código";
+			this->colCodigo->Name = L"colCodigo";
+			// 
+			// colNombre
+			// 
+			this->colNombre->HeaderText = L"Nombre";
+			this->colNombre->Name = L"colNombre";
+			// 
+			// colCapacidad
+			// 
+			this->colCapacidad->HeaderText = L"Capacidad";
+			this->colCapacidad->Name = L"colCapacidad";
+			// 
+			// colUbicacion
+			// 
+			this->colUbicacion->HeaderText = L"Ubicacíon";
+			this->colUbicacion->Name = L"colUbicacion";
+			// 
+			// colNombreEncargado
+			// 
+			this->colNombreEncargado->HeaderText = L"Nombre del encargado";
+			this->colNombreEncargado->Name = L"colNombreEncargado";
+			// 
+			// colTelefonoEncargado
+			// 
+			this->colTelefonoEncargado->HeaderText = L"Teléfono del encargado";
+			this->colTelefonoEncargado->Name = L"colTelefonoEncargado";
+			// 
 			// Salas
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1179, 681);
+			this->ClientSize = System::Drawing::Size(786, 443);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"Salas";
 			this->Text = L"Salas";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
