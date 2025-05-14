be479ca8f019df5be94e0125e2fc3e8938dbf478
jgarcia-2018163 <jgarcia-2018163@kinal.edu.gt>
2025-05-13 18:16:26 -0600

    feat(add) Implements logic for clients

---
+++ ProyectMovies/ClaseClientes.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "ClaseUsuarios.h";
+
+using namespace System;
+
+namespace ProyectMovies {
+
+	public ref class Cliente
+	{
+	private:
+		int codigo;
+		String^ nombre;
+		String^ apellido;
+		String^ dpi;
+		DateTime^ fechaNacimiento;
+		String^ telefono;
+		String^ email;
+		//User^ usuario;
+
+
+	public:
+		// Constructor
+		Cliente(int codigo, String^ nombre, String^ apellido, String^ dpi, DateTime^ fechaNacimiento, String^ telefono, String^ email/*, User^ usuario*/)
+		{
+			this->codigo = codigo;
+			this->nombre = nombre;
+			this->apellido = apellido;
+			this->dpi = dpi;
+			this->fechaNacimiento = fechaNacimiento;
+			this->telefono = telefono;
+			this->email = email;
+
+			//Solo test
+			//this->usuario = usuario;
+		}
+		// Getters y Setters
+		property int Codigo
+		{
+			int get() { return codigo; }
+			void set(int value) { codigo = value; }
+		}
+		property String^ Nombre
+		{
+			String^ get() { return nombre; }
+			void set(String^ value) { nombre = value; }
+		}
+		property String^ Apellido
+		{
+			String^ get() { return apellido; }
+			void set(String^ value) { apellido = value; }
+		}
+		property String^ DPI
+		{
+			String^ get() { return dpi; }
+			void set(String^ value) { dpi = value; }
+		}
+		property DateTime^ FechaNacimiento
+		{
+			DateTime^ get() { return fechaNacimiento; }
+			void set(DateTime^ value) { fechaNacimiento = value; }
+		}
+		property String^ Telefono
+		{
+			String^ get() { return telefono; }
+			void set(String^ value) { telefono = value; }
+		}
+		property String^ Email
+		{
+			String^ get() { return email; }
+			void set(String^ value) { email = value; }
+		}
+		/*property User^ Usuario
+		{
+			User^ get() { return usuario; }
+			void set(User^ value) { usuario = value; }
+		}*/
+	};
+}
